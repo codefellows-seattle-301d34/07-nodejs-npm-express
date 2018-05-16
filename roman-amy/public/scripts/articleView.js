fd345e60d3135d42c5a37f92d4c950692e27320a
Amy Cohen <anbcohen@gmail.com>
2018-05-16 12:04:29 -0700

    fixed Article to article on articleView.js that was causing console.log errors

--- roman-amy/public/scripts/articleView.js
+++ roman-amy/public/scripts/articleView.js
@@ -127,7 +127,7 @@ articleView.submit = event => {
 
   // COMMENT: Where is this function defined? When is this function called? What event ultimately triggers its execution?
   // This is defined on Article.prototype.insertRecord on the article.js. This is triggered unpon submit of the form. It is called/invoked right here, below. 
-  Article.insertRecord();
+  article.insertRecord();
 }
 
 articleView.initIndexPage = () => {
