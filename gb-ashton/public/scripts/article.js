c520540ec9791299c33a5d6504ecd7b0c5305cbe
Ashton K Ellis <ashtonkellis@gmail.com>
2018-05-16 12:02:14 -0700

    finished review and commend tasks

--- gb-ashton/public/scripts/article.js
+++ gb-ashton/public/scripts/article.js
@@ -50,7 +50,7 @@ Article.prototype.insertRecord = function(callback) {
       console.log(data);
 
       // COMMENT: What is the purpose of this line? Is the callback invoked when this method is called? Why or why not?
-      // PUT YOUR RESPONSE HERE
+      // This line will immediately invoke the callback function, only if the callback function exists. This callback is not invoked, because callback is undefined currently.
       if (callback) callback();
     })
 };
