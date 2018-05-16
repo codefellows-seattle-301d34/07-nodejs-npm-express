afb16b9207eb1243fd88efa878ff4313f8c5914c
ria agnes jose <joseagnesria@gmail.com>
2018-05-16 09:49:03 -0700

    completed the 3rd user story

--- ria-tracy/server.js
+++ ria-tracy/server.js
@@ -4,10 +4,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const ROOT ={root:'public'};
 
 
 // public directory holds all the files requierd for the websites and app.use() called below  tells express where to find them.When express gets request for files it fills them from the public folder.
-app.use(express.static('./public'));
+app.use(express.static('public'));
+
+app.get('/new',(req, res)=>{
+  console.log(`request for new.html recieved`);
+  res.sendFile('new.html',ROOT);
+});
 // REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
 app.post('/articles', express.urlencoded(), (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
